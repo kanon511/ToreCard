eff8492533dbef1753a43955bb3a992470958f2e
zhangc <2568382472@qq.com>
2024-03-03 22:08:46 +0800

    使物体移动的代码

---
+++ Assets/Chap/PlayController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour
+{
+    public float runSpeed;
+    public float jumpSpeed;
+    private Rigidbody2D myRigidbody;
+    private BoxCollider2D myFeet;
+    private bool isGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        CheckGrounded();
+    }
+
+    void CheckGrounded()
+    {
+        isGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    }
+    void Run()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector2 playerVal = new Vector2(moveDir * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVal;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGround)
+            {
+                Vector2 jumpvel = new Vector2(0.0f, jumpSpeed);
+                myRigidbody.velocity = Vector2.up * jumpvel;
+            }
+        }
+    }
+}
