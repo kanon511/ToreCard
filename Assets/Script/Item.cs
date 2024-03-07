43cb5c6a4ad9b0b03a58fd279cd9e5c253f8dd05
fzfz <764281407@qq.com>
2024-03-07 22:07:48 +0800

    掉落物

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-[System.Serializable]
 public class Item : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -16,11 +16,16 @@ public class Item : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)//检测碰撞
     {
-        if (collision.gameObject.tag == "Item")
+        if (collision.gameObject.tag == "Item")//如果碰撞到的物体标签为Item
         {
-            Destroy(collision.gameObject);
+            Destroy(collision.gameObject);//删除物体
         }
+    }
+    public static void SummonItem(int itemId, Vector3 position)
+    {
+        GameObject item = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/Item/" + itemId + ".prefab");//定义item（掉落物）为制定路径上的预制体
+        Instantiate(item, position, Quaternion.Euler(0, 0, 0));//在指定位置生成掉落物
     }
 }
--- Assets/Script/SpawnItem.cs
+++ Assets/Script/SpawnItem.cs
@@ -4,24 +4,19 @@ using UnityEngine;
 
 public class SpawnItem : MonoBehaviour
 {
-    public Transform[] SpawnPoints;
-    public float spawnTime = 2.5f;
-    public GameObject Items;// Start is called before the first frame update
+    
+    // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnItems", spawnTime, spawnTime);
+        Item.SummonItem(6, Vector3.zero);//调用Item类的SummonItem方法并传值
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    void SpawnItems()
-    {
-        int spawnIndex = Random.Range(0,SpawnPoints.Length);//随机位置生成掉落物
-        Instantiate(Items, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);//在指定位置生成掉落物
-    }
+    
     
 
 
