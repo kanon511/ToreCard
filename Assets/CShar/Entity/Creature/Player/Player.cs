3c2fabc5c1ef08a7573e6fc9df1ca8d0fe0198e7
Hanppiness <3031483530@qq.com>
2024-03-11 21:54:36 +0800

    实现实体检测子弹碰撞

--- Assets/CShar/Entity/Creature/Player/Player.cs
+++ Assets/CShar/Entity/Creature/Player/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : Entity
-{
+{ 
+
     public float endurance = 200; //“铁骨”，影响生命值以及所受伤害
     public float stength = 200; //“强击”，影响物理伤害
     public float intelligence = 200; //“智力”，影响法术伤害
@@ -23,7 +24,7 @@ public class Player : Entity
         buffList.AddLast(buff);
         Buff buff1 = new Regeneration(this);
         buffList.AddLast(buff1);
-    }
+     }
 
     
 
