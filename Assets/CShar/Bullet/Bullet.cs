ca5b9c9da599d78ccfafe2bd17e5ec465644202b
ws1gaq <3163295299@qq.com>
2024-03-17 17:24:12 +0800

    修改

--- Assets/CShar/Bullet/Bullet.cs
+++ Assets/CShar/Bullet/Bullet.cs
@@ -1,8 +1,14 @@
+using Mono.Cecil;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+
+struct BulletFly
+{
+    Entity entity;
+};
 
 public class Bullet : MonoBehaviour
 {
@@ -11,6 +17,7 @@ public class Bullet : MonoBehaviour
     public Entity entity;
     public float bulletWallDamage = 10f;     //对墙的子弹伤害
     public float bulletCreatureDamage = 20f; //对生物的子弹伤害
+    
 
     void Start()
     {
@@ -32,7 +39,7 @@ public class Bullet : MonoBehaviour
         bullet.speed = direction * 5.0f;
     }
 
-    public void AttackEntity(Entity entity)
+    public void AttackEntity(Entity entity)      //被攻击的实体
     {
         if(entity != null)
          {
@@ -40,7 +47,13 @@ public class Bullet : MonoBehaviour
              {
                  entity.TakeDamage(bulletWallDamage);
              }
-             if(entity is Enemy ||  entity is Player)
+
+             if(this.entity is Player && entity is Entity)    //区别玩家和敌人的子弹
+            {
+                entity.TakeDamage(bulletCreatureDamage);
+            }
+
+             if(entity is Player ||  this.entity is Enemy)
              {
                  entity.TakeDamage(bulletCreatureDamage);
              }
